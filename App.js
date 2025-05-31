5a09d61ad35ee72e57307bb65d8c3f26797a11a8
Liana Karla Capizani <lianacapizani@gmail.com>
2025-05-31 12:03:22 -0300

    organizando a estrutura

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import Routes from './routes';
 
-export default function App() {
+export default function App(){
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+  
+  if (!fontsLoaded) {
+    return null; // ou <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Routes/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  );
+};
---
+++ screens/Home/index.jsx
@@ -0,0 +1,12 @@
+import Text from '../components/atoms/Text';
+
+export default function Home() {
+    return (
+        <>
+        <Text weight="bold">Título importante</Text>
+        <Text>Texto normal</Text>
+        </>
+
+    );
+}
+
